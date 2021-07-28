41021281dc9074c30b9fb8eb83907181608433a0
sudhuser <86882312+sudhuser@users.noreply.github.com>
2021-07-28 19:44:25 +0530

    testing commit for git

--- testHello.java
+++ testHello.java
@@ -3,6 +3,6 @@ public class testHello {
 	public static void main(String args[])
 	{
 		System.out.println("My First Java 16 program");
-	    System.out.println("Thank u fro help, test jenkins trigger");
+	    System.out.println("Thank u fro help, test jenkins trigger, sssssssssssssssss");
 	}
 }	
