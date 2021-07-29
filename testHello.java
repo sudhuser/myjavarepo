9dc9b83bfc1348b78232cc3f09b4c55a1d7be006
sudhansu sabut <sudhansu1628@gmail.com>
2021-07-29 10:55:16 +0530

    deleted testHello from repo

--- testHello.java
+++
@@ -1,8 +0,0 @@
-public class testHello {
-	
-	public static void main(String args[])
-	{
-		System.out.println("My First Java 16 program");
-	    System.out.println("Thank u fro help, test jenkins trigger, ddddddddddddddd");
-	}
-}	
